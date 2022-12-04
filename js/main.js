46749331b7eefef724e4655717a214458da5ae91
willAshC21 <willashc21@gmail.com>
2022-12-04 04:34:44 -0500

    Test Push

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let blue = document.getElementById('');
+let green = document.getElementById('');
+let yellow = document.getElementById('');
+let red = document.getElementById('');
+let orange = document.getElementById('');
+let pink = document.getElementById('');
+
+let bCheck = false;
+let gCheck = false;
+let yCheck = false;
+let rCheck = false;
+let oCheck = false;
+let pCheck = false;
